3472d43dc5abbd95b250f41c994f8acd4f928eb0
Axel <axel.lilja@elev.ga.ntig.se>
2020-09-23 10:05:39 +0200

    First kåmit

---
+++ XML/PowerUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace XML
+{
+    public class PowerUp
+    {
+     public int x = 0;
+     public int y = 0;
+
+     public string name = ""; 
+    
+    
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    3 56 90
+        // PowerUp p = new PowerUp();
+
+        // p.name = "fungi";
+        // p.x = 4; 
+        // p.y = 3; 
+
+        XmlSerializer serializer = new XmlSerializer(typeof(PowerUp));
+
+        FileStream myFile = File.Open("powerup.xml", FileMode.OpenOrCreate);
+
+        PowerUp p = (PowerUp) serializer.Deserialize(myFile);
+
+        myFile.Close();
+
+        System.Console.WriteLine(p.name);
+
+        Console.ReadLine();
+           
+        }
+    }
+}
