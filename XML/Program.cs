aa151dbda503434f6a634c40cee3fcf45034e349
Axel <axel.lilja@elev.ga.ntig.se>
2020-09-23 11:00:01 +0200

    Finito?

--- XML/Program.cs
+++ XML/Program.cs
@@ -10,18 +10,60 @@ namespace XML
         static void Main(string[] args)
         {
             XmlSerializer charSerializer = new XmlSerializer(typeof(Character));
-            Character player = new Character();
+            Character player;
 
-            FileStream file = File.Open(@"char.xml", FileMode.OpenOrCreate);
+            System.Console.WriteLine("Do you want to make a new character? (Y/N)");
+            ConsoleKeyInfo UI = Console.ReadKey();
+            if (UI.Key == ConsoleKey.Y)
+            {
+                player = new Character();
+                Console.Clear();
+                System.Console.WriteLine("How tall is your character?");
+                string input = Console.ReadLine();
+                bool succsess = int.TryParse(input, out player.height);
+                System.Console.WriteLine("How much does your character Weigh?");
+                string input2 = Console.ReadLine();
+                bool succsess2 = int.TryParse(input2, out player.weight);
+                System.Console.WriteLine("What is your characters name?");
+                string name = Console.ReadLine();
+                player.name = name;
 
-            charSerializer.Serialize(file, charSerializer);
 
-            file.Close();
+                FileStream file = File.Open(@"char.xml", FileMode.OpenOrCreate);
+
+                charSerializer.Serialize(file, player);
+
+                file.Close();
+
+
+            }
+            else
+            {
+                using (FileStream file = File.Open(@"char.xml", FileMode.OpenOrCreate))
+                {
+                   player = (Character)charSerializer.Deserialize(file);
+                }
+            }
+           Console.Clear();
+            System.Console.WriteLine("Name: " + player.name);
+            System.Console.WriteLine("Height: " + player.height);
+            System.Console.WriteLine("Weight: " + player.weight);
+
+            Console.ReadLine();
 
 
 
 
-           
+
+
+
+
+
+
+
+
+
         }
+
     }
 }
